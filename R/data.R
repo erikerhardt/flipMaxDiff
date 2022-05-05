31b9aa4d3dca4a2d8c90072d7be1af90c1b4fbaf
Pelishka <pelishk@gmail.com>
2022-05-05 11:44:52 -0500

    Update data.R

--- R/data.R
+++ R/data.R
@@ -64,7 +64,7 @@ IntegrateDesignAndData <- function(design, version, best, worst, seed, questions
 }
 
 #' @importFrom flipData CalibrateWeight CleanSubset CleanWeights
-#' @importFrom flipFormat TrimWhitespace
+#' @importFrom flipU TrimWhitespace
 cleanAndCheckData <- function(design, version = NULL, best, worst, alternative.names, subset = NULL, weights = NULL,
                               characteristics = NULL, seed, questions.left.out = 0)
 {
